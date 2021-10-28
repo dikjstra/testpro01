98205f48a60850c6c388646cb9297ab91f6e3529
Dijkstra <1010557667@qq.com>
2021-10-28 19:45:41 +0800

    first commit

---
+++ src/ArrayTest.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int arr[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        MyTools mt = new MyTools();
+        mt.BubbleSort(arr);
+        Scanner scanner = new Scanner(System.in);//.var 自动分配变量名
+        System.out.println("\n-----排序后------");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+
+class MyTools {
+    public void BubbleSort(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
